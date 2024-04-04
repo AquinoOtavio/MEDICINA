e2cff08f384234900fe2ad3e66182a7c810903fe
Otavio Aquino <132668773+AquinoOtavio@users.noreply.github.com>
2024-04-04 16:41:26 -0300

    .

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+firebaseConfig = {
+  apiKey: "AIzaSyA8Quym29QEQbmrpFvSvUCNxsUDq-q8c-4",
+  authDomain: "crud-a1a10.firebaseapp.com",
+  projectId: "crud-a1a10",
+  storageBucket: "crud-a1a10.appspot.com",
+  messagingSenderId: "131052859028",
+  appId: "1:131052859028:web:a9483454124dca69280e81"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
